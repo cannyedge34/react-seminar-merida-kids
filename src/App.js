2c591d9605042d735e064f954ff353192888b87b
1paciente <1paciente.com@gmail.com>
2017-12-20 14:55:17 +0100

    updating state updating-state-immutably-v6

--- src/App.js
+++ src/App.js
@@ -6,26 +6,34 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 24 },
-      { name: 'John', age: 26 },
-      { name: 'Albert', age: 42 }
+      { id: '1', name: 'Max', age: 24 },
+      { id: '2', name: 'John', age: 26 },
+      { id: '3', name: 'Albert', age: 42 }
     ],
     otherState: 'other value',
     showPersons: false
   };
 
-  nameChangedHandler = event => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 24 },
-        { name: event.target.value, age: 26 },
-        { name: 'Albert', age: 48 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    // Alternative to modern approach
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   deletePersonHandler = personIndex => {
-    const persons = this.state.persons;
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
   };
@@ -57,6 +65,8 @@ class App extends Component {
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
+                key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
